4c8b802659cd048513aa0d9a4c3045a66eb9dafa
Bobby Iveson <18601512+bobbyiveson@users.noreply.github.com>
2026-04-14 13:06:32 +0100

    Improve comments & variable naming for solution

--- algorithms/warm-up/php/solve-me-first/solution.php
+++ algorithms/warm-up/php/solve-me-first/solution.php
@@ -1,19 +1,19 @@
 <?php
 
 // Create a function that returns the sum of two numbers:
-function solveMeFirst($a, $b){
-  return $a + $b;
+function solveMeFirst($num1, $num2) {
+  return $num1 + $num2;
 }
 
 // Open a handle to the STDIN:
 $handle = fopen("php://stdin", "r");
-// Get the first line:
-$_a = fgets($handle);
-// Get the second line:
-$_b = fgets($handle);
+// Get the first number argument:
+$num1 = fgets($handle);
+// Get the second number argument:
+$num2 = fgets($handle);
 // Get the result:
-$sum = solveMeFirst((int)$_a, (int)$_b);
-
+$sum = solveMeFirst((int)$num1, (int)$num2);
+// Print the result to the console:
 echo $sum, PHP_EOL;
 // Always close the handle to free up the resource:
 fclose($handle);
